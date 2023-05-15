16849859283b79b30f27c2be19ab2e8a3dab9869
Nippur7 <nippur.argento@gmail.com>
2023-05-15 20:22:26 -0300

    adding table

--- pages/3_Tabla.py
+++ pages/3_Tabla.py
@@ -1,6 +1,18 @@
 import streamlit as st
 import pandas as pd
 
+tabla_orig = pd.DataFrame()
+columna1 = ["Puntos en la montaña","Dirección de máximo descenso"]
+columna2 = ["(7,8,f(7, 8))","<---,--->"]
+columna3 = ["(-4,1,f(-4, 1))","<---,--->"]
+columna4 = ["(5,10,f(5, 10))","<---,--->"]
+
+tabla_orig['Puntos en el plano'] = columna1
+tabla_orig['P1'] = columna2
+tabla_orig['P2'] = columna3
+tabla_orig['P3'] = columna4
+st.table(tabla_orig)
+
 st.write("P1 = (7, 8): Para encontrar la coordenada z correspondiente a P1, simplemente evaluamos la función f(x, y) en las coordenadas x = 7 y y = 8:")
 st.latex(r'''
     f(7, 8) = 7^2 + 7 + 8^2 = 49 + 7 + 64 = 120
@@ -13,6 +25,18 @@ st.write("P3 = (5, 10): Evaluamos la función f(x, y) en las coordenadas x = 5 y
 st.latex(r'''
     f(5, 10) = 5^2 + 5 + 10^2 = 25 + 5 + 100 = 130
     ''')
+tabla_orig = pd.DataFrame()
+columna1 = ["Puntos en la montaña","Dirección de máximo descenso"]
+columna2 = ["(7,8,f(7, 8))","<---,--->"]
+columna3 = ["(-4,1,f(-4, 1))","<---,--->"]
+columna4 = ["(5,10,f(5, 10))","<---,--->"]
+
+tabla_orig['Puntos en el plano'] = columna1
+tabla_orig['P1'] = columna2
+tabla_orig['P2'] = columna3
+tabla_orig['P3'] = columna4
+st.table(tabla_orig)
+
 
 tabla = pd.DataFrame()
 columna1 = ["Puntos en la montaña","Dirección de máximo descenso"]
